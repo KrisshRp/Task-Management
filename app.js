002761bd4be0a06bd7a3e8090d2fc70e9b358468
KR!$$H <48906449+KrisshRp@users.noreply.github.com>
2024-11-15 15:38:57 +0530

    cors update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 
 require("./Bin/env/env");
 require('./Database/config/connection');
@@ -7,6 +8,7 @@ require("./Database/config/connect").connect();
 
 var app = express();
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
